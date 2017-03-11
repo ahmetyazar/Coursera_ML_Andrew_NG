d58aeff29888f8e2cdb9ddc1a99df1371f4c1635
ahmetyazar <ahmetyazar@yahoo.com>
2017-03-11 13:25:13 -0500

    initial draft

---
+++ ex1/ex1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 11 12:08:22 2017
+
+@author: yazar
+"""
+
+def gradient_descent():
+    """function(a, b) -> list"""
+    print("gradient descent")
+
+gradient_descent()
