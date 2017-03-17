d45e3e5c8be3e356add5af72ee65b7c2b3093df8
ahmetyazar <ahmetyazar@yahoo.com>
2017-03-17 07:46:27 -0400

    added new files

--- assignments/ex1/ex1.py
+++ assignments/ex1/ex1.py
@@ -9,15 +9,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
+def normalize(df):
+    """ Gets pandas Dataframe and returns normalized version
+
+    Keyword arguments:
+    x: input pandas dataframe to be normalized
+    """
+    return (df - df.mean())/ df.std()
+
 def gradient_descent():
     """function(a, b) -> list"""
     print("gradient descent algorithm.")
     print("a new line")
 
 
-train = pd.read_csv('ex1data2.txt', names=['x1','x2','y']) # pylint: disable-msg=C0103
+train = pd.read_csv('ex1data2.txt', names=['x1', 'x2', 'y']) # pylint: disable-msg=C0103
 print('train.shape = {0}'.format(train.shape))
-X = train.ix[:,train.columns != 'y']
+X = train.ix[:, train.columns != 'y']
 print('X.shape = {0}'.format(X.shape))
-y = train['y']
-print('y.shape = {0}'.format(y.shape))
+Y = train['y']
+print('y.shape = {0}'.format(Y.shape))
+
+
