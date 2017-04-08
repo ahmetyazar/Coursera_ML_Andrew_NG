79d097277c06c4759d3f859c40292dd042b891bf
ahmetyazar <ahmetyazar@yahoo.com>
2017-04-08 13:13:55 -0400

    adding ex3

---
+++ assignments/ex3/ex3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 11 12:08:22 2017
+
+@author: yazar
+"""
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+if __name__ == "__main__":
+
+    # load data
